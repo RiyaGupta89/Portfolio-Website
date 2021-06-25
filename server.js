5943eb32fb1c372ad2e030db14c32aca66a15d0b
Riya Gupta git config --global user.email riyag0841@gmail.com <riyaaaaaaaa8@gmail.com>
2021-06-25 16:10:10 +0530

    Minor changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static('public'))
+app.use('/static', express.static('static'));
 
 app.get("/", (req, res)=> {
     res.sendFile(__dirname+"\\index.html");
